2eff00ab09870b515816343c3ce8bfef55e74158
_macrografi <cenk.ozkan@agilatek.com>
2020-11-30 14:53:41 +0300

    fix js

--- src/js/mobile-menu/mobile-menu.js
+++ src/js/mobile-menu/mobile-menu.js
@@ -13,10 +13,6 @@
 
   var defaults = {
     selector: '#Sidenav',
-    /*        effect: 'basic-zoom',
-        speed: '0.6s',
-        card: false,
-        overlay: false,*/
     callback: function () {},
   };
 
@@ -77,42 +73,6 @@
       }
       getWindowWidth();
       window.addEventListener('resize', getWindowWidth);
-      /*            var childCount = items[i].children.length;
-
-            if (childCount >= 2) {
-                items[i].children[1].style.transition = settings.speed;
-            } else {
-                items[i].children[0].style.transition = settings.speed;
-            }*/
-
-      /*            function getCard(elem, event) {
-                if (settings.card) {
-                    if (event === 'mouseover') {
-                        elem.children[0].classList.add('active');
-                    } else {
-                        elem.children[0].classList.remove('active');
-                    }
-                }
-            }*/
-      /*            function getOverlay(elem, event) {
-                if (settings.overlay) {
-                    if (event === 'mouseover') {
-                        elem.children[2].classList.add('active');
-                    } else {
-                        elem.children[2].classList.remove('active');
-                    }
-                }
-            }*/
-
-      /*            items[i].addEventListener('mouseover', function () {
-                this.classList.add(settings.effect);
-                getCard(this, 'mouseover');
-                getOverlay(this, 'mouseover');
-            });
-            items[i].addEventListener('mouseout', function () {
-                getCard(this, 'mouseout');
-                getOverlay(this, 'mouseout');
-            });*/
     }
     settings.callback(items);
   };
